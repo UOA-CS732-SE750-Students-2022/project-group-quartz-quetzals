e9b56eda90cb4184697ff5a8eccc6cdc008e2aca
YaweiZhou <yaweizhou6688@163.com>
2022-05-06 20:28:58 +0800

    RankList and Forum

--- music-player/src/topPage/rankDetail.js
+++ music-player/src/topPage/rankDetail.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Footer from "../pages/footer/Footer";
-
+import RankList from "../pages/rankList/rankList"
 import {useParams} from "react-router-dom";
 
 function RankDetail(){
     let { typeid } = useParams();
     return(
         <div>
-
+           <RankList typeid={typeid}/>
             <Footer/>
         </div>
     );
