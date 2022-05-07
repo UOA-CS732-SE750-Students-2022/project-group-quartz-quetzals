32bdea907116e643cefc387a3efed71c0f256fed
孙龚予 <sungongyu@oceancreate.com>
2022-05-07 20:48:10 +0800

    rank title

--- music-player/src/common/service/artist.js
+++ music-player/src/common/service/artist.js
@@ -5,3 +5,15 @@ export function getArtistList(type,limit){
     url:`/artist/list?type=${type}&area=96&initial=-1&limit=${limit}`
   })
 }
+
+export function getArtistSimilar(id){
+  return request({
+    url:`/simi/artist?id=${id}`
+  })
+}
+
+export function getArtistDescription(id){
+  return request({
+    url:`/artist/detail?id=${id}`
+  })
+}
