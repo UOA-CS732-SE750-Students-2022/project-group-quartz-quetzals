a049e3ed0d42f30a6f4ac8a9555d33e0018aabe5
adamliu327 <adam963082387@gmail.com>
2022-05-08 14:33:08 +1200

    Backend listen together (Radio)

--- backend/src/routes/api/index.js
+++ backend/src/routes/api/index.js
@@ -3,6 +3,8 @@ import express from 'express';
 const router = express.Router();
 
 import comments from './comments';
+import radio from "./radio";
 router.use('/comments', comments);
+router.use('/radio', radio);
 
 export default router;
